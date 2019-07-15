6320aeccb028326b115352bf99b40757e125c044
zxh5566 <1045746362@qq.com>
2019-07-15 08:39:54 +0800

    database

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '101.201.223.149',
     // 数据库名
-    'database'        => 'studyfoxcms',
+    'database'        => 'starcms',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '111111',
+    'password'        => 'vipin905vp',
     // 端口
     'hostport'        => '',
     // 连接dsn
@@ -29,7 +29,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => 'sfox_',
+    'prefix'          => 'star_',
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
